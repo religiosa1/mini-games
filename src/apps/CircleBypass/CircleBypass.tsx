3d8949c5eed32406eb42b80c0413f8320d00c3f1
Viacheslav Azarov <public@religiosa.ru>
2023-05-21 18:06:10 +0200

    Small visual fixes

--- src/apps/CircleBypass/CircleBypass.tsx
+++ src/apps/CircleBypass/CircleBypass.tsx
@@ -6,9 +6,12 @@ import "./CircleBypass.scss";
 
 export function CircleBypass() {
   const [gameState, setGameState] = createSignal<GameStateEnum>(GameStateEnum.ready);
+  const [timeoutDisable, setTimeoutDisable] = createSignal(false);
 
   function handleGameEnd(won: boolean) {
     setGameState(won ? GameStateEnum.won : GameStateEnum.lost);
+    setTimeoutDisable(true);
+    setTimeout(() => setTimeoutDisable(false), 1000);
   }
 
   function moveState() {
@@ -22,7 +25,7 @@ export function CircleBypass() {
     <div class="circle-bypass">
       <Game isRunning={gameState() === GameStateEnum.inProgress} onGameEnd={handleGameEnd} />
       <Show when={gameState() !== GameStateEnum.inProgress}>
-        <button type="button" class="circle-bypass__overlay" onClick={moveState}>
+        <button type="button" disabled={timeoutDisable()} class="circle-bypass__overlay" onClick={moveState}>
           <Switch>
             <Match when={gameState() === GameStateEnum.ready}>
               <Notification>
